12a36d608b3f09bb9950b34467ce67fc356b837d
PR45H <Prashantpillai7@gmail.com>
2025-01-17 07:37:01 +0530

    Add product model with schema definition for MongoDB

---
+++ Server/models/product.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema
+    ({
+        name: { type: String, required: true },
+        type: { type: String, required: true }, // e.g., "bedding", "furniture"
+        price: { type: Number, required: true },
+        description: { type: String },
+        specifications: { type: Map, of: mongoose.Schema.Types.Mixed }, // Dynamic specifications
+        images: [String],
+        warranty: {
+        commercialWarranty: { type: Boolean, default: false },
+        },
+    }, { timestamps: true }, { versionKey: false });
+
+const Product = mongoose.model('product', productSchema);
+module.exports = Product;
