8e1c37071d80653878a5325948d952962bb96b70
PR45H <Prashantpillai7@gmail.com>
2025-01-24 01:42:25 +0530

    Add POST route for adding new products in product router

--- Server/routes/product.route.js
+++ Server/routes/product.route.js
@@ -1,8 +1,9 @@
 const productRouter = require('express').Router();
 const productController = require('../controllers/product.controller');
 
-productRouter.get('/products', productController.allProducts);
-productRouter.get('/product/:type', productController.getProduct);
-productRouter.get('/product/:type/:id', productController.getProductById);
+productRouter.get('/', productController.allProducts);
+productRouter.get('/:type', productController.getProduct);
+productRouter.get('/:type/:id', productController.getProductById);
+productRouter.post('/add-product', productController.addProduct);
 
 module.exports = productRouter;
