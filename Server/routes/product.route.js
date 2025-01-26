dd9cd221a1b394a21b92e7fbcb5969893aa19eb2
PR45H <Prashantpillai7@gmail.com>
2025-01-26 17:27:14 +0530

    Add update and delete routes for products in product router

--- Server/routes/product.route.js
+++ Server/routes/product.route.js
@@ -5,5 +5,7 @@ productRouter.get('/', productController.allProducts);
 productRouter.get('/:type', productController.getProduct);
 productRouter.get('/:type/:id', productController.getProductById);
 productRouter.post('/add-product', productController.addProduct);
+productRouter.post('/update-product/:id', productController.updateProduct);
+productRouter.post('/delete-product/:id', productController.deleteProduct);
 
 module.exports = productRouter;
