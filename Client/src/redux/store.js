2ff36ebb7c11de15fa4365205e698089d12f4cec
PR45H <Prashantpillai7@gmail.com>
2025-01-30 20:35:53 +0530

    Add cart and product reducers to Redux store configuration

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './slices/cartSlice';
+import productReducer from './slices/productSlice';
 
 export const store = configureStore({
     reducer: {
         // Add reducers here
+        cart: cartReducer,
+        product: productReducer,
     },
 });
 
