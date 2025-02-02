5d99c680c31eb16faa4a9ea9dd9f5db64c6e1174
PR45H <Prashantpillai7@gmail.com>
2025-02-03 00:25:06 +0530

    Add ColorModeButton and Chakra UI components to App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import { HStack, Button, VStack } from '@chakra-ui/react'
+import { RiMailLine, RiArrowRightLine } from 'react-icons/ri'
+import { ColorModeButton } from './components/ui/color-mode' //for dark mode 
+import {Outlet} from 'react-router-dom'
 
 function App() {
-  
   return (
     <>
-      <h1>Hello WOrld</h1>
+      <ColorModeButton>
+        
+      </ColorModeButton>
     </>
   )
 }
