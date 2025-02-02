bf4ebc7f97a760d6bf6ac504fcfdab367810b910
PR45H <Prashantpillai7@gmail.com>
2025-02-03 00:25:35 +0530

    Wrap App component with Redux and custom providers for state management

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from './components/ui/provider'
+import { Provider as ReduxProvider } from 'react-redux'
+import store from './redux/store'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <Provider>
+        <App />
+      </Provider>
+    </ReduxProvider>
   </StrictMode>,
 )
