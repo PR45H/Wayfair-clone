5fcd5f2318a182704be60594c1cf9d15a861688a
PR45H <Prashantpillai7@gmail.com>
2025-02-04 02:43:05 +0530

    Implement routing in main.jsx using React Router

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -5,12 +5,24 @@ import App from './App.jsx'
 import { Provider } from './components/ui/provider'
 import { Provider as ReduxProvider } from 'react-redux'
 import store from './redux/store'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+// router
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {index:true}
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ReduxProvider store={store}>
       <Provider>
-        <App />
+        <RouterProvider router={router}/>
       </Provider>
     </ReduxProvider>
   </StrictMode>,
