3d1919b9cddb086978be65c2b782c868f42f7915
PR45H <Prashantpillai7@gmail.com>
2025-02-03 22:49:40 +0530

    Enhance cart slice with initial state and add reducers for adding, removing, and clearing items

--- Client/src/redux/slices/cartSlice.js
+++ Client/src/redux/slices/cartSlice.js
@@ -1,12 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {}
+const initialState = {
+    items: [],
+    totalAmount: 0,
+    totalQuantity: 0,
+    isLoading: false,
+    error: null
+}
 
 const cartSlice = createSlice({
     name: "cart",
     initialState,
     reducers: {
-        // Add reducers here
+        // Add to cart
+        addToCart: (state, action) => {
+            const newItem = action.payload;
+            const existingItem = state.items.find(item => item.id === newItem.id)
+            if (existingItem) {
+                state.totalQuantity += 1
+            } else {
+                state.items.push({...newItem, quantity:1})
+            }
+        },
+        removeFromCart: (state, action) => {
+            
+        },
+        clearCart: (state,action) => {
+            state.items = [];
+            state.totalAmount = 0;
+            state.totalQuantity = 0
+        },
     }
 })
 
