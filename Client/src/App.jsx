f8d26957b772ff6b248b55692de04de298ae6f11
PR45H <Prashantpillai7@gmail.com>
2025-02-04 02:42:58 +0530

    Refactor App component to include Header, Footer, and Toaster components

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,14 +3,19 @@ import './App.css'
 import { HStack, Button, VStack } from '@chakra-ui/react'
 import { RiMailLine, RiArrowRightLine } from 'react-icons/ri'
 import { ColorModeButton } from './components/ui/color-mode' //for dark mode 
-import {Outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import { Toaster } from './components/ui/toaster'
 
 function App() {
   return (
     <>
-      <ColorModeButton>
-        
-      </ColorModeButton>
+      <ColorModeButton />
+        <Header />
+      <Outlet />
+      <Footer />
+      <Toaster/>
     </>
   )
 }
