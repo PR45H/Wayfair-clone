470614dd352c9e736a791030bcb90e902c4bb268
PR45H <Prashantpillai7@gmail.com>
2025-02-03 22:57:58 +0530

    Add auth reducer to Redux store configuration

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './slices/cartSlice';
 import productReducer from './slices/productSlice';
+import authReducer from '@/redux/slices/authSlice'
 
 export const store = configureStore({
     reducer: {
         // Add reducers here
         cart: cartReducer,
         product: productReducer,
+        auth: authReducer
     },
 });
 
