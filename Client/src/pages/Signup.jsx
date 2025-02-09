9fd3790089ba4a541a5306ce4f1fa71006a86c3d
PR45H <Prashantpillai7@gmail.com>
2025-02-09 12:45:55 +0530

    Adjust logo size on Signup page for improved responsiveness

--- Client/src/pages/Signup.jsx
+++ Client/src/pages/Signup.jsx
@@ -10,7 +10,7 @@ const Signup = () => {
             <div className='main_box flex flex-col items-center'>
                 <div className='top_banner w-[80%] items-center flex justify-between border-b border-opacity-30 border-black m-auto p-8'>
                     <div className="logo">
-                        <img src={logo} alt="logo" className='h-14' />
+                        <img src={logo} alt="logo" className='h-7 sm:h-14' />
                     </div>
                     <div className='span flex gap-4'> Secure Login <MdLockOutline className='text-2xl'/></div>
                 </div>
