43de0b8d63c0b003acced8e9a44aab77d1f8396b
PR45H <Prashantpillai7@gmail.com>
2025-02-09 11:38:59 +0530

    Add Toaster component to Provider for improved toast functionality

--- Client/src/components/ui/provider.jsx
+++ Client/src/components/ui/provider.jsx
@@ -2,11 +2,17 @@
 
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import { ColorModeProvider } from './color-mode'
+import { Toaster } from '../ui/toaster'
 
 export function Provider(props) {
   return (
     <ChakraProvider value={defaultSystem}>
       <ColorModeProvider {...props} />
+      {/* Importing Toast here to solve the toaster functionality 
+          was not working outside the app.jsx.
+          so using it in hogher component tree
+    */}
+      <Toaster />         
     </ChakraProvider>
   )
 }
