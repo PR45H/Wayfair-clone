e286624637478f7193b5d03cdf0cf4a525405ed6
PR45H <Prashantpillai7@gmail.com>
2025-02-11 23:31:05 +0530

    no changes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,7 +4,9 @@ import { Outlet } from 'react-router-dom'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 
+
 function App() {
+
   return (
     <>
       <ColorModeButton />
