632b527aef35fa773b03a9e67143f57943a19415
PR45H <Prashantpillai7@gmail.com>
2025-02-11 23:56:28 +0530

    Add UserLoginForm component and update Signin and Home pages to include it

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,8 +1,14 @@
+import HeroBanner from '@/components/layout/HeroBanner'
 import React from 'react'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <div>
+                <HeroBanner />
+            </div>
+            
+        </>
     )
 }
 
--- Client/src/pages/Signin.jsx
+++ Client/src/pages/Signin.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { MdLockOutline } from 'react-icons/md'
+import logo from '@/assets/Wayfair_logo_with_tagline.png'
+import UserLoginForm from '@/components/layout/UserLoginForm'
 
 const Signin = () => {
     return (
-        <div>Signin</div>
+        <>
+        <div className='main_box flex flex-col items-center'>
+                <div className='top_banner w-[80%] items-center flex justify-between border-b border-opacity-30 border-black m-auto p-8'>
+                    <div className="logo">
+                        <img src={logo} alt="logo" className='h-7 sm:h-14' />
+                    </div>
+                    <div className='span flex gap-4'> Secure Login <MdLockOutline className='text-2xl'/></div>
+                </div>
+
+                <div>
+                    <UserLoginForm />
+                </div>
+            </div>
+        </>
     )
 }
 
