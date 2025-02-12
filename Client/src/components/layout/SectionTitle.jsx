7ad68d9d4dbbb7946643c4a10a46e9132cb47f7f
PR45H <Prashantpillai7@gmail.com>
2025-02-12 22:12:35 +0530

    Add SectionTitle component for displaying section headings

---
+++ Client/src/components/layout/SectionTitle.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SectionTitle = ({title}) => {
+    return (
+        <>
+            <div className="heading">
+                <h2 className="text-2xl font-semibold">{title}</h2>
+            </div>
+        </>
+    )
+}
+
+export default SectionTitle
