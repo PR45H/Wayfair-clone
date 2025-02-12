86fadc8473578846dcbd908c6c3f4075b0431767
PR45H <Prashantpillai7@gmail.com>
2025-02-12 22:12:27 +0530

    Add SectionTitle and update layout in Home page

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,13 +1,22 @@
 import HeroBanner from '@/components/layout/HeroBanner'
+import SectionTitle from '@/components/layout/SectionTitle'
 import React from 'react'
+import { FaArrowRight } from 'react-icons/fa'
 
 const Home = () => {
     return (
         <>
             <div>
-                <HeroBanner />
+                <div>
+                    <HeroBanner />
+                </div>
+
+                <div className="container px-4 flex border items-center gap-2 hover:gap-3 cursor-pointer w-fit">
+                    <SectionTitle title={"deals of the day"} />
+                    <FaArrowRight />
+                </div>
             </div>
-            
+
         </>
     )
 }
