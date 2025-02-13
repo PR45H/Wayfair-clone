d05cd5985f4995b9771de8f188d3b4e40305443e
PR45H <Prashantpillai7@gmail.com>
2025-02-13 23:00:34 +0530

    Add TopHeader component to App layout

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,7 @@ import { ColorModeButton } from './components/ui/color-mode' //for dark mode
 import { Outlet } from 'react-router-dom'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
+import TopHeader from './components/layout/TopHeader'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <ColorModeButton />
+      <TopHeader/>
         <Header />
         <Outlet />
         <Footer />
