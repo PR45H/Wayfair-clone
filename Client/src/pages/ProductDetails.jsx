6101b66f8eaa24b9a63149e9f59ba8b3ac9c88a7
PR45H <Prashantpillai7@gmail.com>
2025-02-18 22:52:24 +0530

    init commit

--- Client/src/pages/ProductDetails.jsx
+++ Client/src/pages/ProductDetails.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import productApi from '@/api/product.api'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const ProductDetails = () => {
+    const { id } = useParams()
+    // console.log(id)
+    const [products, setProducts] = useState(null)
+
+
+    // will check id and fetch product details
+    useEffect(() => {
+        const fetchProduct = async () => {
+            try {
+                const response = await productApi.get(`/products/${id}`)
+                console.log(response.data)
+                setProducts(response.data)
+
+            } catch (error) {
+                console.error(error)
+
+            }
+        }
+
+        if (id) {
+            fetchProduct()
+        }
+    }, [id])
+
     return (
         <>
             <div>
-                Product page
+                {products && (
+                    <div>
+                        <h1>{products.title}</h1>
+                        <img src={products.image} alt={products.name} className='h-[450px]'/>
+                        <p>{products.description}</p>
+                        <p>{products.price}</p>
+                    </div>
+                )}
             </div>
         </>
     )
