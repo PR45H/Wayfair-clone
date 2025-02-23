9ffb2f22b038eb9ed24a5f0207d849ca7c50f65f
PR45H <Prashantpillai7@gmail.com>
2025-02-23 21:24:30 +0530

    Enhance ProductDetails page: integrate Header, SectionTitle, ProductAccordion, and ProductRating components for improved layout and user experience

--- Client/src/pages/ProductDetails.jsx
+++ Client/src/pages/ProductDetails.jsx
@@ -1,41 +1,70 @@
 import productApi from '@/api/product.api'
+import Header from '@/components/layout/Header'
+import SectionTitle from '@/components/layout/SectionTitle'
+import ProductAccordion from '@/components/product/ProductAccordion'
+import ProductRating from '@/components/product/ProductRating'
 import React, { useEffect, useState } from 'react'
+import { FaDollarSign } from 'react-icons/fa'
 import { useParams } from 'react-router-dom'
 
 const ProductDetails = () => {
     const { id } = useParams()
-    // console.log(id)
     const [products, setProducts] = useState(null)
 
-
-    // will check id and fetch product details
     useEffect(() => {
         const fetchProduct = async () => {
             try {
                 const response = await productApi.get(`/products/${id}`)
                 console.log(response.data)
                 setProducts(response.data)
-
             } catch (error) {
                 console.error(error)
-
             }
         }
-
-        if (id) {
-            fetchProduct()
-        }
+        if (id) fetchProduct()
     }, [id])
 
     return (
         <>
-            <div>
+            <Header />
+            <div className="p-4 md:p-8">
                 {products && (
-                    <div>
-                        <h1>{products.title}</h1>
-                        <img src={products.image} alt={products.name} className='h-[450px]'/>
-                        <p>{products.description}</p>
-                        <p>{products.price}</p>
+                    <div className="flex flex-col md:flex-row border gap-4 p-4">
+                        <div className="product_image w-full md:w-[30%]">
+                            <img
+                                src={products.image}
+                                alt={products.name}
+                                className="w-full object-contain h-[300px] md:h-[450px]"
+                            />
+                        </div>
+
+                        <div className="flex flex-col gap-4 w-full md:w-[70%]">
+                            <div className="product_title">
+                                <SectionTitle title={products.title} />
+                            </div>
+
+                            <div className="star_rating flex flex-col sm:flex-row gap-3 items-center">
+                                <ProductRating rating={products.rating.rate} />
+                                <div>{products.rating.rate}</div>
+                                <div className="text-[#7B189F] underline hover:no-underline font-semibold">
+                                    {products.rating.count} Reviews
+                                </div>
+                            </div>
+
+                            <div className="product_price">
+                                <span className="font-semibold text-2xl flex items-center">
+                                    <FaDollarSign /> {products.price}
+                                </span>
+                            </div>
+
+                            <div className="product_description w-full md:w-[50%]">
+                                <ProductAccordion
+                                    title="Description"
+                                    content={products.description}
+                                    className="border"
+                                />
+                            </div>
+                        </div>
                     </div>
                 )}
             </div>
