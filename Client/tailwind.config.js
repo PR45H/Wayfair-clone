3f12e1292535333ca03f793bb297ff54edcc7e3b
PR45H <Prashantpillai7@gmail.com>
2025-02-23 21:23:57 +0530

    Update Tailwind CSS configuration: add custom breakpoints for responsive design

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        // Mobile devices
+        mobile: "320px",
+        // Tablets
+        tablet: "768px",
+        // Laptops
+        laptop: "1024px",
+        // Desktops and Large screens
+        desktop: "1280px",
+      },
+    },
   },
   plugins: [],
-}
-
+};
