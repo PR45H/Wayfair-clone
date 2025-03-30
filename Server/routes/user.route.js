fde16380a85c831cf244241c87ab84a34005c320
PR45H <Prashantpillai7@gmail.com>
2025-03-31 01:23:13 +0530

    Update user routes: add authenticated route for retrieving current user information

--- Server/routes/user.route.js
+++ Server/routes/user.route.js
@@ -1,7 +1,9 @@
 const userRouter = require('express').Router();
 const userController = require('../controllers/user.controller');
+const isAuthenticated = require('../middlewares/isAuthenticated');
 
 userRouter.post('/register', userController.register);
 userRouter.post('/login', userController.login);
+userRouter.get('/me', isAuthenticated, userController.getMe);
 
 module.exports = userRouter;
