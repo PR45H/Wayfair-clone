de7e690b8dafea523ee01964ccd3f30d48e60e54
PR45H <Prashantpillai7@gmail.com>
2025-03-31 01:22:41 +0530

    ``` No code changes detected. ```

--- Client/src/pages/Cart.jsx
+++ Client/src/pages/Cart.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Headers from '@/components/layout/Header'
+import axios from 'axios'
+import productApi from '@/api/product.api'
 
 const Cart = () => {
+    const { id } = useParams()
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            try {
+                const response = await productApi.get(`/carts/${id}`)
+                console.log(response.data)
+            } catch (error) {
+                
+            }
+        }
+        if(id) fetchProduct()
+    }, [])
+
     return (
-        <div>Cart</div>
+        <div>
+            {/* Your cart content */}
+        </div>
     )
 }
 
