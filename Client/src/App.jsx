c368ebebfa554ed9bdf353f0149b611aba9e222f
PR45H <Prashantpillai7@gmail.com>
2025-03-31 01:21:48 +0530

    Add AuthCheck component: integrate authentication check in App component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,17 +4,17 @@ import { Outlet } from 'react-router-dom'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import TopHeader from './components/layout/TopHeader'
-
+import AuthCheck from './components/auth/AuthCheck'
 
 function App() {
-
   return (
     <>
+      <AuthCheck />
       <ColorModeButton />
       <TopHeader/>
-        <Header />
-        <Outlet />
-        <Footer />
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   )
 }
